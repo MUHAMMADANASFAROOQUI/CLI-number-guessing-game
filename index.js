b2bd5ee5050090b841f2f9f7843ec821873caddd
Muhammad Anas <anasshafat7@gmail.com>
2024-03-24 18:13:52 +0500

    Created CLI-Number-Guessing-Game

---
+++ index.js
@@ -0,0 +1,16 @@
+#! /src/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Please Guess a Number between 1-10",
+    },
+]);
+if (answer.userGuessedNumber === randomNumber) {
+    console.log("Congratulations,You Guess a Right Number");
+}
+else {
+    console.log("You Guess a Wrong Number, Try Again.");
+}
